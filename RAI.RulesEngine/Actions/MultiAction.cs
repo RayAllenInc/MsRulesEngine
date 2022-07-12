a145dce5651f43173a12794f8542bb2281d8365e
rai-sjanssens <31856682+rai-sjanssens@users.noreply.github.com>
2022-07-12 10:02:08 -0500

    Added MultiAction

---
+++ RAI.RulesEngine/Actions/MultiAction.cs
@@ -0,0 +1,35 @@
+﻿using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RulesEngine.Actions
+{
+    public class MultiAction : ActionBase
+    {
+        private readonly ActionFactory _actionFactory;
+
+        public MultiAction(IDictionary<string, Func<ActionBase>> actionMap)
+        {
+            _actionFactory = new ActionFactory(actionMap);
+        }
+
+        public override async ValueTask<object> Run(ActionContext context, RuleParameter[] ruleParameters)
+        {
+            var actionInfos = context.GetContext<List<ActionInfo>>("Actions");
+            var results = new List<object>();
+
+            foreach (var actionInfo in actionInfos)
+            {
+                var action = _actionFactory.Get(actionInfo.Name);
+                var resultTree = context.GetParentRuleResult();
+                //var ruleParameters = context.GetParentRuleResult().Inputs.Select(kv => new RuleParameter(kv.Key, kv.Value)).ToArray();
+                var result = await action.ExecuteAndReturnResultAsync(new ActionContext(actionInfo.Context, resultTree), ruleParameters, true);
+
+                results.Add(result);
+            }
+
+            return new ValueTask<object>(results.AsReadOnly());
+        }
+    }
+}
